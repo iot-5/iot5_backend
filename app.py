1c39c372ff86f0a17672b593c7759a1f6e47ed34
BSY <45937750+ideawolf@users.noreply.github.com>
2023-05-28 18:34:27 +0900

    reformat and base dataset

--- app.py
+++ app.py
@@ -8,7 +8,12 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
-dataset = []
+dataset = [
+    ('p3', [{'mac': '91:9f:33:5c:23:09', 'rssi': 30}, {'mac': '95:1f:33:ac:35:3a', 'rssi': 50}, {'mac': '96:7f:23:55:29:87', 'rssi': 70}]),
+    ('p1', [{'mac': '91:9f:33:5c:23:02', 'rssi': 20}, {'mac': '95:1f:33:ac:35:32', 'rssi': 70}, {'mac': '96:7f:23:55:29:82', 'rssi': 20}]),
+    ('p2', [{'mac': '91:9f:33:5c:23:02', 'rssi': 40}, {'mac': '95:1f:33:ac:35:32', 'rssi': 80}, {'mac': '96:7f:23:55:29:82', 'rssi': 50}]),
+    ('p4', [{'mac': '91:9f:33:5c:23:11', 'rssi': 40}, {'mac': '95:1f:33:ac:35:11', 'rssi': 80}, {'mac': '96:7f:23:55:29:11', 'rssi': 50}, {'mac': '91:9f:33:5c:23:02', 'rssi': 20}]),
+]
 def calculate_euclidean_distance(pattern, rssi_values):
     distance = np.sqrt(np.sum((np.array(pattern) - np.array(rssi_values)) ** 2))
     return distance
@@ -18,9 +23,11 @@ def calculate_euclidean_distance(pattern, rssi_values):
 def add_point():
     point_data = request.json
     name = point_data['name']
-    data = [ap for ap in point_data['data']]
+    data = [{'mac': ap['mac'], 'rssi': ap['rssi']} for ap in point_data['data']]
     dataset.append((name, data))
+    print(dataset)
     return jsonify({'message': 'Point added successfully.'})
+
 
 @app.route('/getpoint', methods=['POST'])
 def get_point():
@@ -28,11 +35,15 @@ def get_point():
     matched_location = match_pattern(rssi_values)
     return jsonify({'location': matched_location})
 
+
+
 def match_pattern(rssi_values):
     distances = []
     for data in dataset:
-        pattern = [ap[ap_key] for ap in data[1] for ap_key in ap.keys()]
-        distance = calculate_euclidean_distance(pattern, rssi_values)
+        print(data)
+        dataset_ap_values = [ap['rssi'] for ap in data[1]]
+        min_length = min(len(rssi_values), len(dataset_ap_values))
+        distance = calculate_euclidean_distance(dataset_ap_values[:min_length], rssi_values[:min_length])
         distances.append(distance)
 
     min_index = np.argmin(distances)
