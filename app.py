d573c7045876d3b4d125cac092f20fe69bc09669
BSY <45937750+ideawolf@users.noreply.github.com>
2023-05-30 01:44:53 +0900

    Mac을 필터링하고 rssi를 비교함

--- app.py
+++ app.py
@@ -1,47 +1,43 @@
-from flask import Flask, request, jsonify
 import csv
-import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# 데이터 로드
-def load_data():
-    with open('data.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        data = list(reader)
-    return data
+data = []
 
+with open('data.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        data.append(row)
 
-# 데이터 추가 엔드포인트
-@app.route('/addpoint/', methods=['POST'])
-def add_point():
-    point_data = request.get_json()
-    with open('data.csv', 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        for entry in point_data['data']:
-            writer.writerow([point_data['name'], entry['mac'], entry['rssi']])
-    return jsonify({'message': 'Data added successfully.'})
+def calculate_distance(rssi1, rssi2):
+    return abs(rssi1 - rssi2)
 
-@app.route('/getpoint/', methods=['POST'])
+@app.route('/getpoint', methods=['POST'])
 def get_point():
-    data = request.get_json()['data']
-    loaded_data = load_data()
-    predicted_names = []
-    for entry in data:
-        mac = entry['mac']
-        rssi = entry['rssi']
-        matched_points = [point for point in loaded_data if point[1] == mac]
-        if matched_points:
-            distances = [abs(float(point[2]) - float(rssi)) for point in matched_points]
-            min_distance = min(distances)
-            closest_points = [point for point, distance in zip(matched_points, distances) if distance == min_distance]
-            closest_names = [point[0] for point in closest_points]
-            predicted_name = max(set(closest_names), key=closest_names.count)
-            predicted_names.append(predicted_name)
-    return jsonify({'names': predicted_names})
+    json_data = request.get_json()
 
+    mac_rssi_list = json_data['data']
+    user_location = None
+    min_distance = float('inf')
 
+    for location in data:
+        location_name = location[0]
+        location_mac = location[1]
+        location_rssi = int(location[2])
+
+        for item in mac_rssi_list:
+            mac = item['mac']
+            rssi = item['rssi']
+
+            if mac == location_mac:
+                distance = calculate_distance(rssi, location_rssi)
+
+                if distance < min_distance:
+                    min_distance = distance
+                    user_location = location_name
+
+    return jsonify({'user_location': user_location})
 
 if __name__ == '__main__':
     app.run()
