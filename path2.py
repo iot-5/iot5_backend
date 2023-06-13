782dbb6e878676234aa298eb5596f6021ca433cc
Jongseo Lee <jongseo12345@gmail.com>
2023-06-13 22:51:26 +0900

    4층 값 변환

--- path2.py
+++ path2.py
@@ -72,10 +72,10 @@ def add_edges_from_bottom_line(G, bottom_line_nodes):
         x2, y2 = G.nodes[node2]['pos']
         weight = ueclidian_distance(x1, y1, x2, y2)
         G.add_edge(node1, node2, weight=weight)
-        G.add_edge(node1, 1, weight=ueclidian_distance(x1, y1, 900, 1855))
-        G.add_edge(node1, 3, weight=ueclidian_distance(x1, y1, 900, 1635))
-        G.add_edge(node1, 5, weight=ueclidian_distance(x1, y1, 900, 735))
-        G.add_edge(node1, 7, weight=ueclidian_distance(x1, y1, 900, 200))
+        G.add_edge(node1, "1", weight=ueclidian_distance(x1, y1, 909, 1882.5))
+        G.add_edge(node1, "3", weight=ueclidian_distance(x1, y1, 909, 1633.25))
+        G.add_edge(node1, "5", weight=ueclidian_distance(x1, y1, 909, 739.5))
+        G.add_edge(node1, "7", weight=ueclidian_distance(x1, y1, 909, 181))
 
 
 def add_edges_from_top_line(G, top_line_nodes):
@@ -86,10 +86,12 @@ def add_edges_from_top_line(G, top_line_nodes):
         x2, y2 = G.nodes[node2]['pos']
         weight = ueclidian_distance(x1, y1, x2, y2)
         G.add_edge(node1, node2, weight=weight)
-        G.add_edge(node1, 2, weight=ueclidian_distance(x1, y1, 370, 1855))
-        G.add_edge(node1, 4, weight=ueclidian_distance(x1, y1, 430, 1635))
-        G.add_edge(node1, 6, weight=ueclidian_distance(x1, y1, 680, 735))
-        G.add_edge(node1, 8, weight=ueclidian_distance(x1, y1, 830, 200))
+        G.add_edge(node1, "2", weight=ueclidian_distance(x1, y1, 338, 1882.5))
+        G.add_edge(node1, "4", weight=ueclidian_distance(
+            x1, y1, 409.3, 1633.25))
+        G.add_edge(node1, "6", weight=ueclidian_distance(
+            x1, y1, 667.29, 739.5))
+        G.add_edge(node1, "8", weight=ueclidian_distance(x1, y1, 828.5, 181))
 
 
 def calculate_third_side_length(side1, side2, angle):
@@ -112,56 +114,69 @@ def set_nodes():
     with open('rooms.json', 'r') as f:
         data = json.load(f)
 
-    G.add_node(1, pos=(900, 1855))
-    G.add_node(2, pos=(370, 1855))
-    G.add_node(3, pos=(900, 1635))
-    G.add_node(4, pos=(430, 1635))
-    G.add_node(5, pos=(900, 735))
-    G.add_node(6, pos=(680, 735))
-    G.add_node(7, pos=(900, 200))
-    G.add_node(8, pos=(830, 200))
+    G.add_node("1", pos=(909, 1882.5))
+    G.add_node("2", pos=(338, 1882.5))
+    G.add_node("3", pos=(909, 1633.25))
+    G.add_node("4", pos=(409.3, 1633.25))
+    G.add_node("5", pos=(909, 739.5))
+    G.add_node("6", pos=(667.29, 739.5))
+    G.add_node("7", pos=(909, 181))
+    G.add_node("8", pos=(828.5, 181))
+
+    G.add_edge("1", "2", weight=ueclidian_distance(909, 1882.5, 338, 1882.5))
+    G.add_edge("3", "4", weight=ueclidian_distance(
+        909, 1633.25, 409.3, 1633.25))
+    G.add_edge("5", "6", weight=ueclidian_distance(909, 739.5, 667.29, 739.5))
+    G.add_edge("7", "8", weight=ueclidian_distance(909, 181, 828.5, 181))
+
+    G.add_edge("1", "3", weight=ueclidian_distance(909, 1882.5, 909, 1633.25))
+    G.add_edge("3", "5", weight=ueclidian_distance(909, 1633.25, 909, 739.5))
+    G.add_edge("5", "7", weight=ueclidian_distance(909, 739.5, 909, 181))
+    G.add_edge("2", "4", weight=ueclidian_distance(
+        338, 1882.5, 409.3, 1633.25))
+    G.add_edge("4", "6", weight=ueclidian_distance(
+        409.3, 1633.25, 667.29, 739.5))
+    G.add_edge("6", "8", weight=ueclidian_distance(667.29, 739.5, 828.5, 181))
 
     # 엘베 근처노드
-    G.add_node(9, pos=(843, 710))  # 5,6라인
-    G.add_node(10, pos=(773, 710))  # 5,6라인
-    G.add_node(11, pos=(800, 1635))  # 3,4라인
-    G.add_node(12, pos=(800, 1855))  # 1,2라인
+    G.add_node("9", pos=(835, 739.5))  # 5,6라인
+    G.add_node("10", pos=(790.69, 739.5))  # 5,6라인
+    G.add_node("11", pos=(790.69, 1633.25))  # 3,4라인
+    G.add_node("12", pos=(790.69, 1882.5))  # 1,2라인
+
+    G.add_edge("5", "9", weight=ueclidian_distance(909, 739.5, 835, 739.5))
+    G.add_edge("6", "9", weight=ueclidian_distance(667.29, 739.5, 835, 739.5))
+    G.add_edge("5", "10", weight=ueclidian_distance(909, 739.5, 790.69, 739.5))
+    G.add_edge("6", "10", weight=ueclidian_distance(
+        667.29, 739.5, 790.69, 739.5))
+    G.add_edge("3", "11", weight=ueclidian_distance(
+        909, 1633.25, 790.69, 1633.25))
+    G.add_edge("4", "11", weight=ueclidian_distance(
+        409.3, 1633.25, 790.69, 1633.25))
+    G.add_edge("1", "12", weight=ueclidian_distance(
+        909, 1882.5, 790.69, 1882.5))
+    G.add_edge("2", "12", weight=ueclidian_distance(
+        338, 1882.5, 790.69, 1882.5))
 
     # 엘베노드
-    G.add_node(101, pos=(834, 155))  # 1번엘베 8번노드연결
-    G.add_node(102, pos=(845, 710))  # 2번엘베 9번노드연결
-    G.add_node(103, pos=(776, 710))  # 3번엘베 10번노드연결
-    G.add_node(104, pos=(801, 1696))  # 4번엘베 11번노드연결
-    G.add_node(105, pos=(800, 1775))  # 5번엘베 12번노드연결
+    G.add_node("101", pos=(828.5, 146.5))  # 1번엘베 8번노드연결
+    G.add_node("102", pos=(845, 691))  # 2번엘베 9번노드연결
+    G.add_node("103", pos=(790.69, 691))  # 3번엘베 10번노드연결
+    G.add_node("104", pos=(790.69, 1692.5))  # 4번엘베 11번노드연결
+    G.add_node("105", pos=(790.69, 1770))  # 5번엘베 12번노드연결
 
-    G.add_edge(1, 2, weight=ueclidian_distance(900, 1855, 370, 1855))
-    G.add_edge(3, 4, weight=ueclidian_distance(900, 1635, 430, 1635))
-    G.add_edge(5, 6, weight=ueclidian_distance(900, 735, 680, 735))
-    G.add_edge(7, 8, weight=ueclidian_distance(900, 200, 830, 200))
-    G.add_edge(1, 3, weight=ueclidian_distance(900, 1855, 900, 1635))
-    G.add_edge(3, 5, weight=ueclidian_distance(900, 1635, 900, 735))
-    G.add_edge(5, 7, weight=ueclidian_distance(900, 735, 900, 200))
-    G.add_edge(2, 4, weight=ueclidian_distance(370, 1855, 430, 1635))
-    G.add_edge(4, 6, weight=ueclidian_distance(430, 1635, 680, 735))
-    G.add_edge(6, 8, weight=ueclidian_distance(680, 735, 830, 200))
+    G.add_edge("8", "101", weight=ueclidian_distance(828.5, 181, 828.5, 146.5))
+    G.add_edge("9", "102", weight=ueclidian_distance(835, 739.5, 845, 691))
+    G.add_edge("10", "103", weight=ueclidian_distance(
+        790.69, 739.5, 790.69, 691))
+    G.add_edge("11", "104", weight=ueclidian_distance(
+        790.69, 1633.25, 790.69, 1692.5))
+    G.add_edge("12", 105, weight=ueclidian_distance(
+        790.69, 1882.5, 790.69, 1770))
+    G.add_edge("102", "103", weight=ueclidian_distance(845, 691, 790.69, 691))
+    G.add_edge("104", "105", weight=ueclidian_distance(
+        790.69, 1692.5, 790.69, 1770))
 
-    # 엘베노드 연결
-    G.add_edge(5, 9, weight=ueclidian_distance(900, 735, 843, 735))
-    G.add_edge(6, 9, weight=ueclidian_distance(680, 735, 843, 735))
-    G.add_edge(5, 10, weight=ueclidian_distance(900, 735, 773, 735))
-    G.add_edge(6, 10, weight=ueclidian_distance(680, 735, 773, 735))
-    G.add_edge(3, 11, weight=ueclidian_distance(900, 1635, 790, 1635))
-    G.add_edge(4, 11, weight=ueclidian_distance(430, 1635, 790, 1635))
-    G.add_edge(1, 12, weight=ueclidian_distance(900, 1855, 790, 1855))
-    G.add_edge(2, 12, weight=ueclidian_distance(370, 1855, 790, 1855))
-
-    G.add_edge(101, 8, weight=ueclidian_distance(834, 155, 830, 200))
-    G.add_edge(102, 9, weight=ueclidian_distance(845, 708, 843, 735))
-    G.add_edge(103, 10, weight=ueclidian_distance(776, 712, 773, 735))
-    G.add_edge(104, 11, weight=ueclidian_distance(801, 1696, 790, 1635))
-    G.add_edge(105, 12, weight=ueclidian_distance(800, 1775, 790, 1855))
-    G.add_edge(102, 103, weight=ueclidian_distance(845, 708, 776, 712))
-    G.add_edge(104, 105, weight=ueclidian_distance(801, 1696, 800, 1775))
     # Add nodes to the graph
     for room in data['rooms']:
         room_id = room['name']
@@ -183,9 +198,9 @@ def set_nodes():
     for node1, node2 in G.edges():
         G.add_edge(node2, node1, weight=G[node1][node2][0]['weight'])
 
-    # show all edges in the graph
-    for node1, node2 in G.edges():
-        print(node1, node2, G[node1][node2][0]['weight'])
+    # # show all edges in the graph
+    # for node1, node2 in G.edges():
+    #     print(node1, node2, G[node1][node2][0]['weight'])
 
 
 def show_on_image(astar_path):
@@ -193,7 +208,7 @@ def show_on_image(astar_path):
     path_line = []
     for i in range(len(astar_path)):
         path_line.append(G.nodes[astar_path[i]]['pos'])
-    image_path = 'jj.png'
+    image_path = '4th.png'
     image = Image.open(image_path).convert('RGB')
     image_array = np.array(image)
     path_image = draw_path_on_image(image_array, path_line)
@@ -206,6 +221,7 @@ def show_on_image(astar_path):
 
 def result(start, end):
     set_nodes()
+    print(G.edges())
     real_world_scale = 0.04796469368
     initial_way_elevator = 0
     astar_path = nx.astar_path(G, start, end)
@@ -251,16 +267,16 @@ def result(start, end):
                                                                 G.nodes[last_room2]['pos'][0], G.nodes[last_room2]['pos'][1]) * real_world_scale)})
 
     show_on_image(astar_path)
-    # print(final_path)
-    for i in range(len(final_path)):
-        if 'angle' in final_path[i]:
-            # print(i['angle'])
-            if final_path[i]['angle'] < 10 or final_path[i]['angle'] > 350:
-                final_path[i] = {"distance": round(calculate_third_side_length(
-                    final_path[i-1]['distance'], final_path[i+1]['distance'], 180-final_path[i]['angle']))}
-                final_path[i-1] = {'distance': 0}
-                final_path[i+1] = {'distance': 0}
-                i = i+1
+    print("ff: ", final_path)
+    # for i in range(len(final_path)):
+    #     if 'angle' in final_path[i]:
+    #         # print(i['angle'])
+    #         if final_path[i]['angle'] < 10 or final_path[i]['angle'] > 350:
+    #             final_path[i] = {"distance": round(calculate_third_side_length(
+    #                 final_path[i-1]['distance'], final_path[i+1]['distance'], 180-final_path[i]['angle']))}
+    #             final_path[i-1] = {'distance': 0}
+    #             final_path[i+1] = {'distance': 0}
+    #             i = i+1
 
     # delete {"distance": 0} in list, but don't touch "angle"
 
@@ -288,6 +304,7 @@ def result(start, end):
     print(merged_data)
     print(initial_way_elevator)
     return merged_data, initial_way_elevator
+
 
 def result_cli(start, end):
     set_nodes()
